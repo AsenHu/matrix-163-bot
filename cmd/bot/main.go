d350e90e2ebcaeda34dece93462e22ab82db9233
AsenHu <78863300+AsenHu@users.noreply.github.com>
2025-02-04 17:59:46 +0000

    实现歌曲搜索和下载功能，优化缓存检查及错误处理

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
-
-	"maunium.net/go/mautrix"
+	"matrix-163-bot/internal/netease"
 )
 
-// func main() {
-// 	Homeserver := "@user:matrix.org"
-// 	Homeserver_URL, err := mautrix.ParseAndNormalizeBaseURL(Homeserver)
-// 	if err != nil {
-// 		fmt.Println("Error parsing base URL:", err)
-// 		return
-// 	}
-// 	fmt.Println(Homeserver_URL.String())
-// }
+func main() {
+	// 搜索歌曲
+	search_result, err := netease.Search_Song("warma")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println(search_result.Result.Songs[0].Id)
+	// 下载歌曲
+	full_path, err := netease.Download_Song(search_result.Result.Songs[0].Id, "cache")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Downloaded:", full_path)
+}
--- internal/netease/netease.go
+++ internal/netease/netease.go
@@ -6,19 +6,23 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/XiaoMengXinX/Music163Api-Go/api"
 	"github.com/XiaoMengXinX/Music163Api-Go/types"
 	"github.com/XiaoMengXinX/Music163Api-Go/utils"
 )
 
+var data = utils.RequestData{}
+
 func Search_Song(keyword string) (result types.SearchSongData, err error) {
-	data := utils.RequestData{}
+	// 搜索设置
 	Search_Config := api.SearchSongConfig{
 		Keyword: keyword,
 		Limit:   5,
 	}
 
+	// 搜索歌曲
 	Search_Result, err := api.SearchSong(data, Search_Config)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -29,8 +33,18 @@ func Search_Song(keyword string) (result types.SearchSongData, err error) {
 }
 
 func Download_Song(song_id int, path string) (full_path string, err error) {
-	data := utils.RequestData{}
-
+	// 检查歌曲是否在缓存中
+	// 匹配文件名为 song_id-md5
+	full_path = fmt.Sprintf("%s/%d-*", path, song_id)
+	files, err := filepath.Glob(full_path)
+ 	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	if len(files) > 0 {
+		fmt.Println("Song already exists in cache.")
+	}
+	// 获取歌曲下载信息
 	download_info, err := api.GetSongDownloadURL(data, song_id)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -38,19 +52,29 @@ func Download_Song(song_id int, path string) (full_path string, err error) {
 	}
 
 	// 下载歌曲
+	if download_info.Data.Url == "" {
+		err = fmt.Errorf("error: download URL is empty")
+		return
+	}
+	fmt.Println("Downloading:", download_info.Data.Url)
 	resp, err := http.Get(download_info.Data.Url)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
 	defer resp.Body.Close()
-	err = os.Mkdir("cache", 0755)
+	err = os.Mkdir(path, 0755)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	// 下载路径是 cache/song_id-md5
-	full_path = fmt.Sprintf("cache/%d-%s", song_id, download_info.Data.Md5)
+	// 检查 md5 是否存在
+	if download_info.Data.Md5 == "" {
+		fmt.Println("Warning: MD5 is empty.")
+		download_info.Data.Md5 = "empty"
+	}
+	// 下载路径是 path/song_id-md5
+	full_path = fmt.Sprintf("%s/%d-%s", path, song_id, download_info.Data.Md5)
 	out, err := os.Create(full_path)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -58,8 +82,16 @@ func Download_Song(song_id int, path string) (full_path string, err error) {
 	}
 	defer out.Close()
 	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
 	// 验证文件
+	if download_info.Data.Md5 == "empty" {
+		fmt.Println("Warning: MD5 is empty, skip verification.")
+		return full_path, nil
+	}
 	file, err := os.Open(full_path)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -74,7 +106,7 @@ func Download_Song(song_id int, path string) (full_path string, err error) {
 	hashInBytes := hash.Sum(nil)
 	md5_str := fmt.Sprintf("%x", hashInBytes)
 	if md5_str != download_info.Data.Md5 {
-		fmt.Println("Error: MD5 not match.")
+		err = fmt.Errorf("error: MD5 is %s, should be %s", md5_str, download_info.Data.Md5)
 		return
 	}
 
