11bceb6c52853ffa5edc8713f58b11b70541d580
AsenHu <78863300+AsenHu@users.noreply.github.com>
2025-02-08 09:41:23 +0000

    111

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"io"
 	"log"
 	"os"
+	"time"
+
+	"maunium.net/go/mautrix"
+	"maunium.net/go/mautrix/event"
+	"maunium.net/go/mautrix/id"
 )
 
 type Account struct {
@@ -15,22 +20,79 @@ type Account struct {
 	Token    string `json:"token"`
 }
 
+const CONFIG_PATH = "config.json"
+
 func main() {
 	// 检查本地是否有账户状态
-	// 创建文件
-	file, err := os.Open("config.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
 	// 读取文件
-	buffer, err := io.ReadAll(file)
+	buffer, err := os.ReadFile(CONFIG_PATH)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// 解析文件
 	var account Account
 	if err := json.Unmarshal(buffer, &account); err != nil {
+		log.Fatal(err)
+	}
+	// 打印账户信息
+	log.Printf("Username: %s", account.Username)
+	log.Printf("Password: %s", account.Password)
+	log.Printf("BaseURL: %s", account.BaseURL)
+	log.Printf("DeviceID: %s", account.DeviceID)
+	log.Printf("Token: %s", account.Token)
+
+	// 开始登陆
+	log.Printf("Try to login...")
+	client, err := mautrix.NewClient(account.BaseURL, id.UserID(account.Username), account.Token)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 90*time.Second)
+	defer cancel()
+	resp, err := client.Login(ctx, &mautrix.ReqLogin{
+		Type:                     "m.login.password",
+		Password:                 account.Password,
+		Token:                    account.Token,
+		DeviceID:                 id.DeviceID(account.DeviceID),
+		InitialDeviceDisplayName: "163 Music Bot",
+		RefreshToken:             true,
+		Identifier: mautrix.UserIdentifier{
+			Type: "m.id.user",
+			User: account.Username,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	account.Username = resp.UserID.String()
+	account.DeviceID = resp.DeviceID.String()
+	account.Token = resp.AccessToken
+	account.BaseURL = resp.WellKnown.Homeserver.BaseURL
+	log.Printf("Login success")
+	log.Printf("Username: %s", account.Username)
+	log.Printf("DeviceID: %s", account.DeviceID)
+	log.Printf("Token: %s", account.Token)
+	log.Printf("BaseURL: %s", account.BaseURL)
+	// 保存账户状态
+	buffer, err = json.MarshalIndent(account, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := os.WriteFile(CONFIG_PATH, buffer, 0600); err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Save account status success")
+
+	// 设置回调函数
+	syncer := mautrix.NewDefaultSyncer()
+	syncer.OnEventType(event.EventMessage, func(ctx context.Context, ev *event.Event) {
+		log.Printf("Message: %s", ev.Content.AsMessage().Body)
+	})
+	client.Syncer = syncer
+
+	// 启动同步
+	log.Printf("Start sync...")
+	if err := client.Sync(); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -18,7 +18,7 @@ require (
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
 	go.mau.fi/util v0.8.4 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
-	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
-	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/exp v0.0.0-20250207012021-f9890c6ad9f3 // indirect
+	golang.org/x/sys v0.30.0 // indirect
 )
